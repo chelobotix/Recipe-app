4417da46524884506a5c036817ee1c0ad8e02e3a
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-05-30 15:13:43 -0400

    Add recipes show delete actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,7 @@ class RecipesController < ApplicationController
   # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.all
+    puts @recipes
   end
 
   # GET /recipes/1 or /recipes/1.json
@@ -25,24 +26,9 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully created." }
-        format.json { render :show, status: :created, location: @recipe }
+        format.html { redirect_to user_recipe_path(id: @recipe), notice: "Recipe was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /recipes/1 or /recipes/1.json
-  def update
-    respond_to do |format|
-      if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
-        format.json { render :show, status: :ok, location: @recipe }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -52,8 +38,7 @@ class RecipesController < ApplicationController
     @recipe.destroy
 
     respond_to do |format|
-      format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to user_recipes_path, notice: "Recipe was successfully destroyed." }
     end
   end
 
@@ -65,6 +50,7 @@ class RecipesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def recipe_params
-      params.fetch(:recipe, {})
+      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public).merge(
+                                                                                                     user_id: current_user.id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :foods
+  get 'users/index'
+  get 'users/show'
   devise_for :users
+
+  resources :users, only: [:index, :show] do
+    resources :recipes
+    resources :foods
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
