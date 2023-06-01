e8a0f6ea7734d5c73ece358684735864b538c4c4
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-06-01 16:58:38 -0400

    Add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:warning] = exception.message
+    redirect_to recipes_path
+  end
+
   protected
 
   def update_allowed_parameters
