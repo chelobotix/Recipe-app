1f92024db7b632db84aa9b91a6e2b8ee232e3a2e
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-02 15:00:11 -0300

    Create system specs for Foods

--- spec/integration/foods_spec.rb
+++ spec/integration/foods_spec.rb
@@ -28,6 +28,29 @@ RSpec.describe 'Foods', type: :system do
           expect(page).to have_content(food.price.to_s)
         end
       end
+
+      it 'it can create a new food' do
+        user = FactoryBot.create(:user)
+        user.confirm
+
+        visit new_user_session_path
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: user.password
+        click_button 'Log in'
+
+        expect(page).to have_current_path(root_path)
+        expect(page).to have_content('Signed in successfully.')
+
+        visit foods_path
+
+        click_link 'New food'
+
+        fill_in 'Name', with: Faker::Food.dish
+        fill_in 'Quantity', with: Faker::Number.between(from: 1, to: 100)
+        fill_in 'Measurement unit', with: Faker::Food.metric_measurement
+        fill_in 'Price', with: Faker::Number.between(from: 1, to: 100)
+        click_button 'Create Food'
+      end
     end
 
     context 'when user is not logged in' do
